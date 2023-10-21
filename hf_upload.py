7fa8c719356a2e52b807af5c3448bc16072829ab
seonglae <sungle3737@gmail.com>
2023-10-21 14:25:51 +0900

    refactor: hf -> hf_upload.py with indexing function

---
+++ hf_upload.py
@@ -0,0 +1,18 @@
+import time
+
+import fire
+import chromadb
+
+
+def embedding(model_id: str = 'intfloat/multilingual-e5-large',
+                     upload: bool = False, db: str = 'chroma',
+                     prefix: str = 'query: ') -> str:
+  return ''
+
+
+def markdown(upload: bool = False) -> str:
+  return ''
+
+
+if __name__ == '__main__':
+  fire.Fire()
